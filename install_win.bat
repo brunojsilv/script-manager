f7306fce16a3a6c0c0c877f62889019c3d6314dd
Bruno Silva <brunojsilv@gmail.com>
2019-12-06 01:22:34 -0300

    corrigida sintaxe

--- install_win.bat
+++ install_win.bat
@@ -1,4 +1,4 @@
 net user /ADD lxadmin keDBD6V4chcG /ACTIVE:YES /FULLNAME:"LXVOL Share User"
 mkdir C:\Windows\LXVOL
-MKlink %USERPROFILE%\Desktop\LXVOL C:\Windows\LXVOL\
+MKlink /d %USERPROFILE%\Desktop\LXVOL C:\Windows\LXVOL\
 net share LXVOL=C:\Windows\LXVOL /grant:lxadmin,read
