21dd2d7e401cb6c92698497a3026cab2e9202b90
Bruno Silva <brunojsilv@gmail.com>
2019-12-06 20:36:28 -0300

    removida estrutura de pastas && git push origin master

--- install_server_windows/install_win.bat
+++ install_server_windows/install_win.bat
@@ -1,6 +1,4 @@
 net user /ADD lxadmin keDBD6V4chcG /ACTIVE:YES /FULLNAME:"LXVOL Share User"
 mkdir C:\Windows\LXVOL
-mkdir C:\Windows\LXVOL\sh
-mkdir C:\Windows\LXVOL\misc
 MKlink /d %USERPROFILE%\Desktop\LXVOL C:\Windows\LXVOL\
 net share LXVOL=C:\Windows\LXVOL /grant:lxadmin,read
