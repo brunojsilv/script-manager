ef8271287abe350666d76bdc5a7eebf27f8868cc
Bruno Silva <brunojsilv@gmail.com>
2019-12-06 22:13:26 -0300

    Validaçoes adicionadas

--- join_ad_ubuntu/pbis.sh
+++ join_ad_ubuntu/pbis.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-clear
+
+resposta=1
 
 ISROOT(){
   USUARIO=`whoami`
-  if [ "$USUARIO" != "root" ]; then 
+  if [ "$USUARIO" != "root" ]; then
+    clear
     echo "ESTE PROGRAMA PRECISA SER EXECUTADO COM PERMISSOES DE SUPERUSUARIO!" 
     echo "Abortando..." 
     exit 1
@@ -11,42 +13,78 @@ ISROOT(){
 }
 
 INSTALL() {
-
+  clear
   wget -O - http://repo.pbis.beyondtrust.com/apt/RPM-GPG-KEY-pbis|sudo apt-key add -
   wget -O /etc/apt/sources.list.d/pbiso.list http://repo.pbis.beyondtrust.com/apt/pbiso.list
   apt-get update
   apt-get install -y pbis-open
   /opt/pbis/bin/config AssumeDefaultDomain true
-  echo "Instalado!";
-  
+  echo -e "\nInstalaçao concluida!\n\nPresione ENTER para continuar...";
+  read
 }
 
 UNINSTALL(){
-
+  clear
   apt-get remove -y pbis-open
-  echo "Desinstalado!";
-
+  echo -e "\nDesinstalaçao concluida!\n\nPresione ENTER para continuar...";
+  read
 }
 
 JOIN_AD() {
-
+  saida=1
+  clear
   domainjoin-cli join --disable ssh
-  echo "Ingressado!";
-  
+  while [ $saida != 0 ]
+  do
+  echo -e "\nDeseja reiniciar agora? \n[1 = Sim]\n[0 = Não|Cancelar]"
+	read confirma
+
+  if [ $confirma == 0 ]; then
+    saida=0
+    echo -e "\nReinicio cancelado! \n\nPresione ENTER para continuar..."
+    read
+  elif [ $confirma == 1 ]; then
+    saida=0
+    reboot
+  else
+    INVALID
+  fi
 }
 
 LEAVE_AD(){
-
+  saida=1
+  clear
   domainjoin-cli leave
-  echo "Desingressado!";
+  while [ $saida != 0 ]
+  do
+  echo -e "\nDeseja reiniciar agora? \n[1 = Sim]\n[0 = Não|Cancelar]"
+	read confirma
 
+  if [ $confirma == 0 ]; then
+    saida=0
+    echo -e "\nReinicio cancelado! \n\nPresione ENTER para continuar..."
+    read
+  elif [ $confirma == 1 ]; then
+    saida=0
+    reboot
+  else
+    INVALID
+  fi
 }
 
-echo " - Power Broker Identity Services - Open Edition ";
+INVALID(){
+  clear
+	echo -e "Opção inválida! \n\nPresione ENTER para continuar..."
+	read
+}
 
 ISROOT
 
-echo -e “O que deve ser feito? \n[1 = INSTALAR PBIS-OPEN] \n[2 = DESINSTALAR PBIS-OPEN] \n[3 = INGRESSAR NO AD] \n[4 = SAIR DO AD]”
+while [ $resposta != 0 ]
+do
+
+echo -e " - Power Broker Identity Services - Open Edition -\n\n"
+echo -e "O que deve ser feito? \n[1 = INSTALAR PBIS-OPEN] \n[2 = DESINSTALAR PBIS-OPEN] \n[3 = INGRESSAR NO AD] \n[4 = SAIR DO AD] \n[0 = SAIR]"
 read resposta
 
 case "$resposta" in
@@ -62,10 +100,12 @@ case "$resposta" in
   4)
     LEAVE_AD
   ;;
-  *)
-    echo "Opção inválida"
-    echo "Abortando..."
+  0)
   ;;
-esac
+	*)
+    INVALID
+	;;
+  esac
+done
 
-rm -rf install
+clear
